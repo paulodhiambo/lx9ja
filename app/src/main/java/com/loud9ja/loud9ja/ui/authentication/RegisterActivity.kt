44d02e2a5a769cfc8dc6ec3b5c9a89a1c3ad68f1
Paul Odhiamo <paulodhiambo962@gmail.com>
2021-07-20 12:17:45 +0300

    adding trending item recyclerview

--- app/src/main/java/com/loud9ja/loud9ja/ui/authentication/RegisterActivity.kt
+++ app/src/main/java/com/loud9ja/loud9ja/ui/authentication/RegisterActivity.kt
@@ -1,12 +1,25 @@
 package com.loud9ja.loud9ja.ui.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.loud9ja.loud9ja.R
+import com.loud9ja.loud9ja.databinding.ActivityRegisterBinding
+import com.loud9ja.loud9ja.ui.home.MainActivity
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.btnEmailRegistration.setOnClickListener {
+            startActivity(Intent(this, EmailRegistrationActivity::class.java))
+        }
     }
 }
