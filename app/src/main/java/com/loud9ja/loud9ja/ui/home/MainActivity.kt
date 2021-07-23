c38aea864fe6c0f2037b3fec343eb178f924b5f1
Paul Odhiamo <paulodhiambo962@gmail.com>
2021-07-23 18:21:58 +0300

    drawer menu actions setup

--- app/src/main/java/com/loud9ja/loud9ja/ui/home/MainActivity.kt
+++ app/src/main/java/com/loud9ja/loud9ja/ui/home/MainActivity.kt
@@ -11,9 +11,12 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -22,28 +25,36 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.loud9ja.loud9ja.R
 import com.loud9ja.loud9ja.databinding.ActivityMainBinding
+import com.loud9ja.loud9ja.ui.authentication.LoginActivity
+import com.loud9ja.loud9ja.ui.discusion.DiscussionDetailFragment
 import com.loud9ja.loud9ja.ui.livestream.LiveStreamActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomAppBar: BottomAppBar
     private lateinit var bottomSheetDialog: BottomSheetDialog
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        val navView: NavigationView = binding.navView
+        navView = binding.navView
         navView.setupWithNavController(navController)
-        val drawerLayout: DrawerLayout = binding.drawerLayout
+        drawerLayout = binding.drawerLayout
+
+
+        navView.setNavigationItemSelectedListener(this)
+        navView.bringToFront()
 
         binding.drawerButton.setOnClickListener {
             drawerLayout.openDrawer(Gravity.LEFT)
@@ -104,5 +115,52 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(this, LiveStreamActivity::class.java))
                 bottomSheetDialog.dismiss()
             }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_discussions -> {
+                navController.navigate(R.id.action_to_FirstFragment)
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.nav_reports -> {
+                navController.navigate(R.id.action_to_SecondFragment)
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.nav_groups -> {
+                navController.navigate(R.id.action_to_GroupFragment)
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.nav_go_live -> {
+                showBottomDialog()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.nav_sos -> {
+                navController.navigate(R.id.action_to_SOSFragment)
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.nav_polls -> {
+                true
+            }
+            R.id.nav_profile -> {
+                true
+            }
+            R.id.nav_logout -> {
+                drawerLayout.closeDrawer(GravityCompat.START)
+                val intent = Intent(this, LoginActivity::class.java)
+                    .apply {
+                        this.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        this.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    }
+                startActivity(intent)
+                true
+            }
+            else -> false
+        }
     }
 }
