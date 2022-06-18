1a0c5d52d6f1162ad47a31bd09fe28673549dc18
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-06-18 20:25:45 +0300

    update

--- app/src/main/java/com/loud9ja/loud9ja/utils/Constants.kt
+++ app/src/main/java/com/loud9ja/loud9ja/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.loud9ja.loud9ja.utils
 
 object Constants {
     val BASE_URL = "http://loud9ja.travelinnovators.biz/"
+    val IMAGE_PATH = "https://loud9ja.travelinnovators.biz/storage/"
 }
